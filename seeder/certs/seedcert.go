5ce427b025e5f4e425842ef2ffe7c464b6d05e2c
dipghoshraj <dipghoshraj@gmail.com>
2026-01-14 16:42:56 +0530

    update the cert generation script

--- seeder/certs/seedcert.go
+++ seeder/certs/seedcert.go
@@ -1,3 +1,53 @@
 package main
 
 // need you add the cert creation lib from mem-sdk
+import (
+	"log"
+	"os"
+
+	"github.com/odio4u/mem-sdk/certengine/pkg"
+	"gopkg.in/yaml.v3"
+)
+
+type Seeder struct {
+	IP   string `yaml:"ip"`
+	Port string `yaml:"port"`
+	Dns  string `yaml:"dns"`
+	Name string `yaml:"name"`
+}
+
+type Config struct {
+	Version string `yaml:"version"`
+	Seeder  Seeder `yaml:"Seeder"`
+}
+
+var YamlConfig *Config
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	routerIps := []string{YamlConfig.Seeder.IP}
+	dns := []string{YamlConfig.Seeder.Dns}
+
+	_, err := pkg.GenerateSelfSignedGPR(YamlConfig.Seeder.Name, routerIps, dns)
+	pkg.Must(err)
+
+	log.Println("All operations completed successfully.")
+}
+
+func init() {
+	data, err := os.ReadFile("seeder-config.yaml")
+	log.Println("load the config")
+
+	if err != nil {
+		log.Fatalf("error reading YAML file: %v", err)
+	}
+
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		log.Fatalf("error parsing YAML file: %v", err)
+	}
+
+	YamlConfig = &config
+
+}
--- seeder/go.mod
+++ seeder/go.mod
@@ -6,13 +6,15 @@ require (
 	github.com/google/btree v1.1.3
 	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0
 	github.com/odio4u/agni-schema v0.0.2
+	github.com/odio4u/mem-sdk/certengine v0.0.0-20260114102312-83d1080aacaa
 	google.golang.org/grpc v1.78.0
 	google.golang.org/protobuf v1.36.11
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
 
 require (
-	golang.org/x/net v0.47.0 // indirect
-	golang.org/x/sys v0.38.0 // indirect
-	golang.org/x/text v0.31.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20251029180050-ab9386a59fda // indirect
+	golang.org/x/net v0.49.0 // indirect
+	golang.org/x/sys v0.40.0 // indirect
+	golang.org/x/text v0.33.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20260112192933-99fd39fd28a9 // indirect
 )
